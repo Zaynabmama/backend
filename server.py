5735d8f1eb0c7051308d59a9da4d0546c005317c
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-07-07 21:18:27 +0300

    updating code in the backend

--- server.py
+++ server.py
@@ -6,5 +6,5 @@ config_file = os.path.join(os.path.dirname(__file__), '../fullstack-repo/config.
 with open(config_file, 'r') as f:
     config = yaml.safe_load(f)
 
-server_ip = config.get('ServerIPAddress')
-print(f"Starting server at IP address: {server_ip}")
+server = config.get('ServerIPAddress')
+print(f"Starting server at IP address: {server}")
