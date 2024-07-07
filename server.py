5bc9adf2c02dfb575419285092eb69e8f51b605e
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-07-07 21:02:32 +0300

    adding server conf

--- server.py
+++ server.py
@@ -1,1 +1,10 @@
-print('backend')
+import yaml
+import os
+
+config_file = os.path.join(os.path.dirname(__file__), '../fullstack-repo/config.yaml')
+
+with open(config_file, 'r') as f:
+    config = yaml.safe_load(f)
+
+server_ip = config.get('ServerIPAddress')
+print(f"Starting server at IP address: {server_ip}")
